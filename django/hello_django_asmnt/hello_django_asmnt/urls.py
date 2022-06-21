eb374d5cb70c4fb8246cce127ef2c724f1459b6f
dritter44 <dreither44@gmail.com>
2022-06-21 18:32:09 -0500

    commits with all functions and paths working. need to add error codes, and find out how to get return paragraphs to display the parameters that might be too long.

--- django/hello_django_asmnt/hello_django_asmnt/urls.py
+++ django/hello_django_asmnt/hello_django_asmnt/urls.py
@@ -13,41 +13,62 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from http.client import HTTPResponse
+from urllib import response
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
-def rectangleRoute(request):
-    response = HttpResponse("<h1>this shows the route to rectangle</h1>")
+def rectangleAreaRoute(request,length=None,width=None):
+    if length == None and width == None:
+        queryLength = request.GET.get('length')
+        queryWidth = request.GET.get('width')
+        if queryLength == None or queryWidth == None:
+            return HTTPResponse('<h1>404</h1>')
+            # return response.status_code     #find a way to return an HTTP status
+        area = int(queryLength)*int(queryWidth)
+    else:
+        area = length*width
+    response = HttpResponse(f'<h1>The Area of a rectangle with {length} and {width} is {area}.</h1>')
+    return response   
+
+def circleAreaRoute(request,radius=None):
+    if radius == None:
+        queryRadius = request.GET.get('radius')
+        area = (314*((int(queryRadius**2))))/100
+    else:
+        area = ((radius**2)*314)/100
+    response = HttpResponse(f'<h1>The area of a circle with radius {radius} is {area}.</h1>')
     return response
 
-def circleRoute(request):
-    response = HttpResponse("<h1>this shows the route to circle</h1>")
+def rectanglePerimeterRoute(request, length = None, width = None):
+    if length == None and width == None:
+        queryLength = request.GET.get('length')
+        queryWidth = request.GET.get('width')
+        perim = (2*(int(queryLength)))+(2*(int(queryWidth)))
+    else:
+        perim = 2*length + 2*width
+    response = HttpResponse(f'<h1>The perimeter of a rectangle with length {length} and width {width} is {perim}</h1>')
     return response
 
-def rectangleAreaRoute(request, length, width):
-    area = length*width
-    response = HttpResponse(f'<h1>The Area of a rectangle is {area}.</h1>')
-    return response
-    
-    
-    return response
-
-def circleAreaRoute(request):
-    return response
-
-def rectanglePerimeterRoute(request):
-    return response
-
-def circlePerimeterRoute(request):
+def circlePerimeterRoute(request, radius = None):
+    if radius == None:
+        queryRadius = request.GET.get('radius')
+        #add error log here
+        perim = (2*314*int(queryRadius))/100
+    else:
+        perim = (2*314*radius)/100
+    response = HttpResponse(f'<h1>The perimeter of a circle of radius {queryRadius} is {perim}.</h1>')
     return response
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rectangle/', rectangleRoute),
-    path('circle/', circleRoute),
-    path('rectangle/area/<int:length>/<int:width>', rectangleAreaRoute),
-    path('circle/area/', circleAreaRoute),
+    path('rectangle/area/', rectangleAreaRoute),     #format for using the query method
     path('rectangle/perimeter/', rectanglePerimeterRoute),
+    path('circle/area/', circleAreaRoute),
     path('circle/perimeter/',circlePerimeterRoute),
+    path('rectangle/perimeter/<int:length>/<int:width>', rectanglePerimeterRoute),
+    path('rectangle/area/<int:length>/<int:width>', rectangleAreaRoute),
+    path('circle/area/<int:radius>', circleAreaRoute),
+    path('circle/perimeter/<int:radius>',circlePerimeterRoute)
 ]
